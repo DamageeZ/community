7abe7eb2637c94fed4f06ec58d5e919e8f511e10
DamageeZ <zhouzhm1@outlook.com>
2021-05-29 01:17:57 +0800

    init repos

---
+++ src/main/java/com/mapsiz/dev/community/Controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.mapsiz.dev.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author: DamageeZ
+ * @Create: 05-29-2021 0:58
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
